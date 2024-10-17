72cbe8aa046ddeb0c3ddc4f0772b662d67c32c75
Aritra Dey <adey01027@gmail.com>
2024-10-18 00:58:21 +0530

    dev branch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,19 +6,21 @@ import ServiceIcons from '@/components/ServiceIcons';
 import { Carousel } from '@/components/ui/apple-cards-carousel'; // Ensure this path is correct
 import projects, { Project } from '../utils/project'; // Import the project data and Project type
 import Popup from '@/components/Popup';
+import Image from 'next/image'; // Import the Image component from Next.js
 
 const Home: React.FC = () => {
     // Generate carousel items from project data
-    const carouselItems = projects.map((project: Project, index: number) => (
-        <div key={index} className="rounded-3xl bg-white p-4 shadow-md w-80 md:w-[384px]"> {/* Set item width */}
-            <img
+    const carouselItems = projects.map((project: Project) => (
+        <div key={project.id} className="rounded-3xl bg-white p-4 shadow-md w-80 md:w-[384px]"> {/* Set item width */}
+            <Image
                 src={project.src}
                 alt={project.title}
                 className="w-full h-50 object-cover rounded-t-3xl"
+                width={384} // Set an appropriate width
+                height={300} // Set an appropriate height
             />
             <h2 className="text-xl font-bold mt-2 text-blue-600">{project.title}</h2> {/* Update title color here */}
             <p className="text-gray-600">{project.category}</p>
-            {/* Change description to indicate it reflects the project */}
             <p className="mt-4 text-blue-600 italic">Description: {project.description}</p> {/* Emphasize the description */}
         </div>
     ));
